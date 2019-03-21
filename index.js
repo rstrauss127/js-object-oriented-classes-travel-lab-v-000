178fdbf2fb6b96d6e8a6e5a6027c5c11e3a910f6
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-21 17:50:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+class Driver {
+   constructor(name, startDate) {
+     this.name = name;
+     this.startDate = new Date(startDate);
+   }
 
+   startDate() {
+     return startDate;
+   }
+
+   yearsExperienceFromBeginningOf(year) {
+     return year-this.startDate.getFullYear();
+   }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    const hblocks = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal)-eastWest.indexOf(this.endingLocation.horizontal));
+    const vblocks = Math.abs(this.beginningLocation.vertical-this.endingLocation.vertical);
+    return hblocks+vblocks;
+  }
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled()/2;
+    }
+    return this.blocksTravelled()/3;
+  }
+}
