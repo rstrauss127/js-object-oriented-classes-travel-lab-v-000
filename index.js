4175f86690bf2668ca25ce81d234c64c48a2a9f7
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-21 17:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ class Driver {
      return year-this.startDate.getFullYear();
    }
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+  }
+  
+
+  
+  blocksTravelled
+}
